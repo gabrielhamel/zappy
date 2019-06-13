2560f94f52b320ddc212f090d6fde681de2d587d
gabrielhamel <gabriel.hamel@epitech.eu>
2019-06-14 01:26:34 +0200

    Check team names in parsing

--- server/src/parsing/team_name.c
+++ server/src/parsing/team_name.c
@@ -9,6 +9,14 @@
 
 bool check_team(const char *dim)
 {
-    (void)dim;
-    return (true);
+    static char *buff[4096] = {0};
+    static size_t i = 0;
+
+    if (strlen(dim) == 0)
+        return false;
+    for (size_t u = 0; buff[u]; u++)
+        if (!strcmp(buff[u], dim))
+            return false;
+    buff[i++] = (char *)dim;
+    return true;
 }
