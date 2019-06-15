fc965b9981c121c9ccda8e1253edc9b4d8129227
gabrielhamel <gabriel.hamel@epitech.eu>
2019-06-15 19:21:21 +0200

    Fixed sst 0

--- server/src/commands/graphic/sst.c
+++ server/src/commands/graphic/sst.c
@@ -11,12 +11,18 @@
 void cmd_graph_sst(sock_t *cli, sock_list_t *list, char **arg, zarg_t *zarg)
 {
     char buff[4096] = {0};
+    float nb;
 
     if (array_lenght(arg) < 2) {
         dprintf(cli->fd, "sbp\n");
         return;
     }
-    zarg->freq = atof(arg[1]);
+    nb = atof(arg[1]);
+    if (nb <= 0) {
+        dprintf(cli->fd, "sbp\n");
+        return;
+    }
+    zarg->freq = nb;
     dprintf(cli->fd, "sst %.0f\n", zarg->freq);
     sprintf(buff, "sgt %.0f\n", zarg->freq);
     send_all_graphics(list, buff);
