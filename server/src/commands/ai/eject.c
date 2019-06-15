f9d321fc8196d9a714edb50c3d1e64d4a6b69df2
gabrielhamel <gabriel.hamel@epitech.eu>
2019-06-15 17:43:31 +0200

    Eject

--- server/src/commands/ai/eject.c
+++ server/src/commands/ai/eject.c
@@ -7,8 +7,56 @@
 
 #include "graph_commands.h"
 #include "server.h"
+#include "ia_commands.h"
+
+static void send_res(sock_list_t *list, sock_t *cli, size_t nb)
+{
+    char buff[4096] = {0};
+
+    if (nb == 0)
+        dprintf(cli->fd, "ko\n");
+    else {
+        dprintf(cli->fd, "ok\n");
+        sprintf(buff, "pex %d\n", cli->fd);
+        send_all_graphics(list, buff);
+    }
+}
+
+static void eject(sock_list_t *list, ia_t *ejecter, ia_t *ejected)
+{
+    int k = 0;
+
+    ia_move(list, ejected, ejecter->ori);
+    if (ejecter->ori == NORTH)
+        k = 7;
+    if (ejecter->ori == EAST)
+        k = 5;
+    if (ejecter->ori == SOUTH)
+        k = 3;
+    if (ejecter->ori == WEAST)
+        k = 1;
+    dprintf(ejected->id, "eject: %d\n", k);
+}
 
 void cmd_ia_eject(sock_t *cli, sock_list_t *list, char **arg, zarg_t *zarg)
 {
+    zappy_client_t *client;
+    ia_t *me = ZAPPY_CLIENT(cli)->client.ia;
+    size_t nb = 0;
+    ia_t *ia;
 
+    (void)arg;
+    (void)zarg;
+    for (sock_node_t *node = list->start; node != NULL; node = node->next) {
+        client = node->socket->data;
+        if (node->socket->type != CLIENT || client == NULL ||
+        client->cli_type != IA)
+            continue;
+        ia = client->client.ia;
+        if (ia->x != me->x || ia->y != me->y)
+            continue;
+        nb++;
+        eject(list, me, ia);
+    }
+    send_res(list, cli, nb);
 }
