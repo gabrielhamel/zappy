a4ef44680aea23c7c83fb2776e2cac0460fa8b78
gabrielhamel <gabriel.hamel@epitech.eu>
2019-06-20 14:30:46 +0200

    Reapir

--- server/src/sockets/write.c
+++ server/src/sockets/write.c
@@ -12,11 +12,10 @@
 int sock_write(const sock_t *sock, const char *format, ...)
 {
     va_list ap;
-    va_start(ap, format);
     int res;
 
+    va_start(ap, format);
     res = vdprintf(sock->fd, format, ap);
-    fflush(fdopen(sock->fd, "r"));
     va_end(ap);
     return res;
 }
